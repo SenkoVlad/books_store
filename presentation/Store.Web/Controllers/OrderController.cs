1d070b7f4419d2a597dc5a1a11886ec9c2065402
SenkoVlad <vlad.senko2011@yandex.ru>
2020-12-22 10:47:17 +0300

    исправить ошибки на уровне представления

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -29,7 +29,7 @@ namespace Store.Web.Controllers
 
             return RedirectToAction("Index", "Book", new { id = bookId });
         }
-        public IActionResult AddItem(int bookId, int count)
+        public IActionResult AddItem(int bookId, int count = 1)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
 
